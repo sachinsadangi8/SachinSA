cd6c0308f8edba0bd09fb79b95196d712f75db35
sachinsadangi8 <sachin.sadangi@ptgindia.com>
2018-09-24 14:52:24 +0530

    commit

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ package selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Demo 
+public class hello
 {
   public static void main(String[] args)
   {
